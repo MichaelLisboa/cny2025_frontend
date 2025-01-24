91ccd6c50b16980c3d1a1e79a16bbc5fcf9e1eb7
Michael Lisboa <michael.lisboa@gmail.com>
2025-01-25 00:18:04 +0100

    add layout props and styled component

--- src/pages/blank-page.js
+++ src/pages/blank-page.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import styled from "styled-components";
+
+const DivContainer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+`;
 
 const BlankPage = () => {
   return (
     <Layout
       image="background-zodiac-sky.jpg"
-      alignImage={alignImage}
+      alignImage={"bottom"}
       scrollable="true"
     >
-      <div style={{ padding: '20px', textAlign: 'center' }}>
+      <DivContainer>
         <h1>Blank Page</h1>
         <p>This is a blank page.</p>
-      </div>
+      </DivContainer>
     </Layout>
   );
 };
