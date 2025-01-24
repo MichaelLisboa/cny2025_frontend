af9684e34d6011b94fdc31f951274761bd4abbdb
Michael Lisboa <michael.lisboa@gmail.com>
2025-01-24 16:58:25 +0100

    angry.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,10 @@
-// gatsby-node.js
 exports.onCreateWebpackConfig = ({ actions, stage }) => {
+    console.log('onCreateWebpackConfig called with stage:', stage);
     if (stage === 'build-javascript') {
-        console.log('Building JavaScript', stage, actions);
+        console.log('Building JavaScript', stage);
         actions.setWebpackConfig({
             devtool: 'source-map',
         });
+        console.log('Webpack config set with source-map for build-javascript stage');
     }
 };
