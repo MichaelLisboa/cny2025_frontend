132f9b713b4da45509cc043be64d179ef4a434e7
Michael Lisboa <michael.lisboa@gmail.com>
2025-01-24 17:09:00 +0100

    comments for .map

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,10 +1,10 @@
 exports.onCreateWebpackConfig = ({ actions, stage }) => {
     console.log('onCreateWebpackConfig called with stage:', stage);
     if (stage === 'build-javascript') {
-        console.log('Building JavaScript', stage);
+        console.log('SOURCE-MAP: Building JavaScript', stage);
         actions.setWebpackConfig({
             devtool: 'source-map',
         });
-        console.log('Webpack config set with source-map for build-javascript stage');
+        console.log('SOURCE-MAP: Webpack config set with source-map for build-javascript stage');
     }
 };
