e74d326b0590f0f5e6c3e9c074eb5e2fe993790e
Michael Lisboa <michael.lisboa@gmail.com>
2025-01-29 10:31:42 +0100

    point analytics to michaellisboa.com account

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          "G-8LESK85K1S", // Google Analytics / GA
+          "G-WS2KRK4L6B", // Google Analytics / GA
         ],
         // This object is used for configuration specific to this plugin
         pluginConfig: {
