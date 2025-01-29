60482e878a62c5a703952c20134409181224b564
Michael Lisboa <michael.lisboa@gmail.com>
2025-01-29 14:14:51 +0100

    fix layout for iPhones

--- src/components/layout.js
+++ src/components/layout.js
@@ -23,7 +23,6 @@ const Content = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: flex-end;
   width: 100vw;
   height: 100vh;
   box-sizing: border-box;
