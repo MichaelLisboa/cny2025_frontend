189c67085b0a8b547bfd13e2a0649127782b48a3
Michael Lisboa <michael.lisboa@gmail.com>
2025-01-29 22:32:05 +0100

    skip the view-lanterns for now until finished debugging

--- src/pages/index.js
+++ src/pages/index.js
@@ -115,7 +115,7 @@ const IndexPage = () => {
             </p>
           </TextDiv>
           <ButtonWrapper ref={buttonWrapperRef}>
-            <Button variant="glow" text="Continue" onClick={() => navigate('/view-lanterns')} />
+            <Button variant="glow" text="Continue" onClick={() => navigate('/create-lantern')} />
           </ButtonWrapper>
         </ContentContainer>
       </div>
