3734aaf1790e7571cc1b150dac1b5f90df9ce5eb
Michael Lisboa <michael.lisboa@gmail.com>
2025-01-29 07:14:51 +0100

    add gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,20 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-styled-components',
-    // "gatsby-plugin-google-gtag",
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-8LESK85K1S", // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      },
+    },
     {
       resolve: `gatsby-plugin-layout`,
       options: {
