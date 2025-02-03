0123cfede7cdcd87dfde13c686824b01b90b55f1
Michael Lisboa <michael.lisboa@gmail.com>
2025-02-03 16:29:28 +0100

    added animal grids to fortune

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import { StyleSheetManager } from "styled-components";
 import "./src/styles/global.css";
 import "./src/styles/lantern.css";
+import "./src/styles/fortune.css";
 
 export const wrapRootElement = ({ element }) => {
   console.log('Client hydration started');
--- src/pages/fortune.js
+++ src/pages/fortune.js
@@ -27,9 +27,74 @@ import useAnimateTextSequence from "../hooks/useAnimateTextSequence";
 import DatePicker from "../components/datePicker";
 import useAppState from "../hooks/useAppState";
 import { determineZodiacAnimalAndElement } from "../utils/getZodiacAnimal";
+import styled from 'styled-components';
 
 gsap.registerPlugin(Observer);
 gsap.registerPlugin(TextPlugin);
+
+const CompatibleSignsGrid = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 16px;
+  justify-content: center;
+  margin-top: 40px; /* Default margin for desktop */
+
+  flex-wrap: nowrap;
+
+  @media (max-width: 768px) {
+    margin-top: 40px; /* Adjust margin for mobile */
+  }
+
+  @media (min-width: 1920px) {
+    margin-top: 56px; /* Adjust margin for HD screens */
+  }
+`;
+
+const CompatibleSignWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 24px;
+  flex-wrap: nowrap;
+
+  .animal-name {
+    margin-top: 12px;
+    color: white;
+    font-size: 1rem;
+
+    @media (max-width: 768px) {
+      font-size: 0.75rem;
+    }
+
+    @media (min-width: 1920px) {
+      font-size: 1rem;
+    }
+  }
+`;
+
+const CompatibleSignPill = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 80px;
+  height: 80px;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 50%;
+  margin-bottom: 16px;
+
+  flex-wrap: nowrap;
+
+  @media (max-width: 768px) {
+    width: 60px;
+    height: 60px;
+  }
+
+  @media (min-width: 1920px) {
+    width: 100px;
+    height: 100px;
+  }
+`;
 
 const ZodiacPresentation = ({ zodiac, element }) => {
 
@@ -64,6 +129,16 @@ const ZodiacPresentation = ({ zodiac, element }) => {
     [data]
   );
 
+  const getAnimalImageByName = useCallback(
+    (name) => {
+      const image = data.allFile.edges.find(({ node }) =>
+        node.relativePath.includes(`zodiac-${name.toLowerCase()}.png`)
+      );
+      return image ? getImage(image.node.childImageSharp) : null;
+    },
+    [data]
+  );
+
   const elementImage = element ? getImageByName(`element-${element.toLowerCase()}.png`) : null;
   const zodiacImage = zodiac ? getImageByName(`zodiac-${zodiac.toLowerCase()}.png`) : null;
 
@@ -76,6 +151,8 @@ const ZodiacPresentation = ({ zodiac, element }) => {
     const textElements = Array.from(document.querySelectorAll(".fortune-title, .fortune-body"));
     animateRef.current = textElements;
   }, []);
+
+  console.log("currentZodiac", currentZodiac.mostCompatible);
 
   return (
     <div className="fortune-container">
@@ -140,35 +217,103 @@ const ZodiacPresentation = ({ zodiac, element }) => {
           {currentZodiac.relationshipDescription || "No relationship info available."}
         </p>
       </div>
+      <div className="fortune-body-section">
+        <h3 className="fortune-title inline-header text-white">
+          Most Compatible Signs
+        </h3>
+        <CompatibleSignsGrid>
+          {currentZodiac.mostCompatible
+            ? (Array.isArray(currentZodiac.mostCompatible)
+                ? currentZodiac.mostCompatible
+                : currentZodiac.mostCompatible.split(",")
+              ).map((sign, index) => {
+                const animalImage = getAnimalImageByName(sign.trim());
+                return (
+                  <CompatibleSignWrapper key={index}>
+                    <CompatibleSignPill>
+                      {animalImage ? (
+                        <GatsbyImage
+                          image={animalImage}
+                          alt={`${sign.trim()} zodiac`}
+                          placeholder="blurred"
+                          layout="fixed"
+                          width={70}
+                          height={70}
+                        />
+                      ) : (
+                        sign.trim()
+                      )}
+                    </CompatibleSignPill>
+                    <div className="animal-name">{sign.trim()}</div>
+                  </CompatibleSignWrapper>
+                );
+              })
+            : "No compatibility info available."}
+        </CompatibleSignsGrid>
+      </div>
+      <div className="fortune-body-section">
+        <h3 className="fortune-title inline-header text-white">
+          Least Compatible Signs
+        </h3>
+        <CompatibleSignsGrid>
+          {currentZodiac.leastCompatible
+            ? (Array.isArray(currentZodiac.leastCompatible)
+                ? currentZodiac.leastCompatible
+                : currentZodiac.leastCompatible.split(",")
+              ).map((sign, index) => {
+                const animalImage = getAnimalImageByName(sign.trim());
+                return (
+                  <CompatibleSignWrapper key={index}>
+                    <CompatibleSignPill>
+                      {animalImage ? (
+                        <GatsbyImage
+                          image={animalImage}
+                          alt={`${sign.trim()} zodiac`}
+                          placeholder="blurred"
+                          layout="fixed"
+                          width={70}
+                          height={70}
+                        />
+                      ) : (
+                        sign.trim()
+                      )}
+                    </CompatibleSignPill>
+                    <div className="animal-name">{sign.trim()}</div>
+                  </CompatibleSignWrapper>
+                );
+              })
+            : "No compatibility info available."}
+        </CompatibleSignsGrid>
+      </div>
     </div>
   );
 };
 
 const FortunePage = () => {
-  const { state, dispatch, birthdateExists } = useAppState();
-  const [flowState, setFlowState] = useState('idle');
-  const [localBirthdate, setLocalBirthdate] = useState(null);
-  const [localZodiac, setLocalZodiac] = useState(null);
-  const [localElement, setLocalElement] = useState(null);
-  const [isScrolling, setIsScrolling] = useState(true);  // Add state to track scrolling
+  const {state, dispatch, birthdateExists} = useAppState();
+      const [flowState, setFlowState] = useState('idle');
+      const [localBirthdate, setLocalBirthdate] = useState(null);
+      const [localZodiac, setLocalZodiac] = useState(null);
+      const [localElement, setLocalElement] = useState(null);
+      const [isScrolling, setIsScrolling] = useState(true);  // Add state to track scrolling
 
   useEffect(() => {
     if (birthdateExists() && state.zodiac && state.element) {
-      setFlowState('done');
+        setFlowState('done');
       setIsScrolling(true);
     }
   }, [birthdateExists, state.zodiac, state.element]);
 
   const handleNextClick = () => {
-    setFlowState('transitioning');
+        setFlowState('transitioning');
   };
 
   const handleDateSelected = (selectedDate) => {
-    setLocalBirthdate(selectedDate);
+        setLocalBirthdate(selectedDate);
 
-    const { animal, element } = determineZodiacAnimalAndElement(selectedDate);
-    setLocalZodiac(animal);
-    setLocalElement(element);
+      const {animal, element} = determineZodiacAnimalAndElement(selectedDate);
+      setLocalZodiac(animal);
+      setLocalElement(element);
   };
 
   useEffect(() => {
@@ -176,81 +321,81 @@ const FortunePage = () => {
       const timeline = gsap.timeline();
 
       timeline.to(
-        ".background-image",
-        {
-          top: 0,
-          duration: 5,
-          ease: "power2.inOut",
+      ".background-image",
+      {
+        top: 0,
+      duration: 5,
+      ease: "power2.inOut",
         },
-        "<"
+      "<"
       );
 
       timeline.to(".date-picker", {
         y: "350%",
-        opacity: 0,
-        duration: 1.5,
-        ease: "power2.inOut",
+      opacity: 0,
+      duration: 1.5,
+      ease: "power2.inOut",
         onComplete: () => {
-          dispatch({ type: "SET_BIRTHDATE", payload: localBirthdate });
-          dispatch({ type: "SET_ZODIAC", payload: localZodiac });
-          dispatch({ type: "SET_ELEMENT", payload: localElement });
+        dispatch({ type: "SET_BIRTHDATE", payload: localBirthdate });
+      dispatch({type: "SET_ZODIAC", payload: localZodiac });
+      dispatch({type: "SET_ELEMENT", payload: localElement });
         },
       },
-        "-=4.5"
+      "-=4.5"
       );
 
       timeline.fromTo(
-        ".fortune-container",
-        {
-          y: "-100%",
-          opacity: 0
+      ".fortune-container",
+      {
+        y: "-100%",
+      opacity: 0
         },
-        {
-          y: "0%",
-          opacity: 1,
-          duration: 2,
-          ease: "power3.out",
-          clearProps: "transform",
+      {
+        y: "0%",
+      opacity: 1,
+      duration: 2,
+      ease: "power3.out",
+      clearProps: "transform",
           onComplete: () => {
-            setFlowState('done');
-            setIsScrolling(true);
+        setFlowState('done');
+      setIsScrolling(true);
           }
         },
-        "-=2"
+      "-=2"
       );
     }
   }, [flowState]);
 
-  const alignImage = flowState === 'done' ? "top" : "bottom";
+      const alignImage = flowState === 'done' ? "top" : "bottom";
 
-  return (
-    <Layout
-      image="background-zodiac-sky.jpg"
-      alignImage={alignImage}
-      scrollable={isScrolling}
-    >
-      {flowState !== 'done' && (
-        <div className="date-picker">
-          <DatePicker
-            birthdateExists={localBirthdate}
-            handleNextClick={handleNextClick}
-            onDateSelected={handleDateSelected}
-            title="Enter Your Birthdate"
-            paragraphText="Discover your fortune for the new year!"
-            buttonLabel="Next"
+      return (
+      <Layout
+        image="background-zodiac-sky.jpg"
+        alignImage={alignImage}
+        scrollable={isScrolling}
+      >
+        {flowState !== 'done' && (
+          <div className="date-picker">
+            <DatePicker
+              birthdateExists={localBirthdate}
+              handleNextClick={handleNextClick}
+              onDateSelected={handleDateSelected}
+              title="Enter Your Birthdate"
+              paragraphText="Discover your fortune for the new year!"
+              buttonLabel="Next"
+            />
+          </div>
+        )}
+        {flowState !== 'idle' && (
+          <ZodiacPresentation
+            zodiac={flowState === 'done' ? state.zodiac : localZodiac}
+            element={flowState === 'done' ? state.element : localElement}
           />
-        </div>
-      )}
-      {flowState !== 'idle' && (
-        <ZodiacPresentation
-          zodiac={flowState === 'done' ? state.zodiac : localZodiac}
-          element={flowState === 'done' ? state.element : localElement}
-        />
-      )}
-    </Layout>
-  );
+        )}
+      </Layout>
+      );
 };
 
-export default FortunePage;
+      export default FortunePage;
 
 export const Head = () => <SEO title="Fortune" />;
